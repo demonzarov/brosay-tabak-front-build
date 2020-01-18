7bf6c8b22fad08e983ca30ddd2f445cde4c2451f
Dmitry Smirnov <demonzarov@ya.ru>
2020-01-18 18:32:42 +0300

    1.0.8

---
+++ precache-manifest.f9ee7eb952c71386a283c7f1dcb7b9a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "211fb98e316b5d3e0f8aa04f112edb5f",
+    "url": "/brosay-tabak-front-build/index.html"
+  },
+  {
+    "revision": "d9bb8d256900b826c9e9",
+    "url": "/brosay-tabak-front-build/static/css/2.266626f9.chunk.css"
+  },
+  {
+    "revision": "ee273e3de89c727a8021",
+    "url": "/brosay-tabak-front-build/static/css/main.0307ed3d.chunk.css"
+  },
+  {
+    "revision": "d9bb8d256900b826c9e9",
+    "url": "/brosay-tabak-front-build/static/js/2.665b3ecf.chunk.js"
+  },
+  {
+    "revision": "ee273e3de89c727a8021",
+    "url": "/brosay-tabak-front-build/static/js/main.bad7b954.chunk.js"
+  },
+  {
+    "revision": "cc72d5b553d5f45e584d",
+    "url": "/brosay-tabak-front-build/static/js/runtime-main.ac64d719.js"
+  },
+  {
+    "revision": "8e7e08046c053a115db4bc4caacccdad",
+    "url": "/brosay-tabak-front-build/static/media/app_icon_150.8e7e0804.jpg"
+  }
+]);
